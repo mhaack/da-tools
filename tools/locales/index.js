61b7a8367050205594fc3a260d859926e7380d95
Markus Haack <mhaack@adobe.com>
2025-09-01 11:28:25 +0200

    fix: initial locale plugin

---
+++ tools/locales/index.js
@@ -0,0 +1,93 @@
+import { Queue } from 'https://da.live/nx/public/utils/tree.js';
+
+const AEM_ORIGIN = 'https://admin.hlx.page';
+const DA_ORIGIN = 'https://admin.da.live';
+const LANG_CONF = '/.da/translate-v2.json';
+
+export const [setContext, getContext] = (() => {
+  let ctx;
+  return [
+    (supplied) => {
+      ctx = (() => {
+        const { org, repo: site, path, token } = supplied;
+        return { org, site, path, token };
+      })();
+      return ctx;
+    },
+    () => ctx,
+  ];
+})();
+
+export async function getLangsAndLocales() {
+  const { org, site, token } = getContext();
+  const opts = { headers: { Authorization: `Bearer ${token}` } };
+  const resp = await fetch(`${DA_ORIGIN}/source/${org}/${site}${LANG_CONF}`, opts);
+  if (!resp.ok) return { message: { text: 'There was an error fetching languages.', type: 'error' } };
+  const sheet = await resp.json();
+  const { data: langData } = sheet.languages;
+  const { data: localeData } = sheet.locales;
+
+  const langs = langData.map((row) => ({ name: row.name, location: row.location }));
+
+  const locales = localeData.map((row) => {
+    const localeLangs = langs.map((lang) => ({
+      name: lang.name,
+      globalLocation: lang.location,
+      location: `${lang.location}-${row.location.replace('/', '')}`,
+    }));
+    return {
+      ...row,
+      langs: localeLangs,
+    };
+  });
+
+  return { langs, locales };
+}
+
+export async function getPage(fullpath) {
+  const { token } = getContext();
+  const opts = { headers: { Authorization: `Bearer ${token}` } };
+  const resp = await fetch(`${DA_ORIGIN}/source${fullpath}.html`, opts);
+  return resp.status === 200;
+}
+
+export async function copyPage(sourcePath, destPath) {
+  const { token } = getContext();
+  const body = new FormData();
+  body.append('destination', `${destPath}.html`);
+  const opts = { method: 'POST', body, headers: { Authorization: `Bearer ${token}` } };
+  await fetch(`${DA_ORIGIN}/copy${sourcePath}.html`, opts);
+}
+
+export async function publishPages(pages) {
+  const { org, site, token } = getContext();
+  const opts = { method: 'POST', headers: { Authorization: `Bearer ${token}` } };
+
+  const publish = async (url) => {
+    let resp = await fetch(`${AEM_ORIGIN}/preview/${org}/${site}/main${url.path}`, opts);
+    if (resp.status === 200) {
+      resp = await fetch(`${AEM_ORIGIN}/live/${org}/${site}/main${url.path}`, opts);
+    }
+    url.status = resp.status;
+  };
+
+  const queue = new Queue(publish, 5);
+
+  return new Promise((resolve) => {
+    const throttle = setInterval(() => {
+      const nextUrl = pages.find((url) => !url.inProgress);
+      if (nextUrl) {
+        nextUrl.inProgress = true;
+        queue.push(nextUrl);
+      } else {
+        console.log('out');
+        console.log(pages);
+        const finished = pages.every((url) => url.status);
+        if (finished) {
+          clearInterval(throttle);
+          resolve(pages);
+        }
+      }
+    }, 250);
+  });
+}
---
+++ tools/locales/locales.js
@@ -0,0 +1,138 @@
+import { html, LitElement, nothing } from 'da-lit';
+import DA_SDK from 'https://da.live/nx/utils/sdk.js';
+import getStyle from 'https://da.live/nx/public/utils/styles.js';
+import { setContext, getLangsAndLocales, getPage, copyPage, publishPages } from './index.js';
+
+// NX Base
+const nx = `${new URL(import.meta.url).origin}/nx`;
+
+// Styles
+const sl = await getStyle('styles.css');
+const styles = await getStyle(import.meta.url);
+
+class NxLocales extends LitElement {
+  static properties = {
+    _langs: { state: true },
+    _locales: { state: true },
+    _message: { state: true },
+  };
+
+  connectedCallback() {
+    super.connectedCallback();
+    this.shadowRoot.adoptedStyleSheets = [sl, styles];
+    this.setup();
+  }
+
+  async setup() {
+    const { message, langs, locales } = await getLangsAndLocales();
+    if (message) {
+      this._message = message;
+      return;
+    }
+    this._langs = langs;
+    this._locales = locales;
+  }
+
+  findInLang(langs) {
+    return langs.find((item) => this.path.startsWith(`${item.location}/`));
+  }
+
+  flattenLocaleLangs() {
+    return this._locales.reduce((acc, locale) => {
+      acc.push(...locale.langs);
+      return acc;
+    }, []);
+  }
+
+  findCurrentLang() {
+    let found = this.findInLang(this._langs);
+    if (!found) {
+      const flatLocaleLangs = this.flattenLocaleLangs();
+      found = this.findInLang(flatLocaleLangs);
+    }
+    return found;
+  }
+
+  async handleOpen(lang) {
+    const found = this.findCurrentLang();
+    if (!found) return;
+
+    const copyFromLocation = found.globalLocation || found.location;
+    const copyFromPath = this.path.replace(found.location, copyFromLocation);
+    const newPath = this.path.replace(found.location, lang.location);
+    const newFullPath = `/${this.org}/${this.site}${newPath}`;
+    const exists = await getPage(newFullPath);
+    if (!exists) await copyPage(`/${this.org}/${this.site}${copyFromPath}`, newFullPath);
+    this.actions.setHash(newFullPath);
+  }
+
+  async handlePublish(items) {
+    this._message = { text: 'Publishing banner' };
+    const found = this.findCurrentLang();
+    const publishLangs = items[0].langs ? this.flattenLocaleLangs(items) : items;
+    const pageList = publishLangs.map((lang) => ({ path: `${this.path.replace(found.location, lang.location)}` }));
+    await publishPages(pageList);
+    this._message = undefined;
+  }
+
+  renderLocaleLangs(langs) {
+    return html`<div class="locale-lang-list-container">
+      <ul class="locale-lang-group-list">
+        ${langs.map((lang) => html`
+          <li>
+            <p>${lang.name}</p>
+            <button @click=${() => this.handleOpen(lang)}>Edit</button>
+          </li>
+        `)}
+      </ul>
+    </div>`;
+  }
+
+  renderGroup(title, items) {
+    return html`
+      <div class="lang-group">
+        <div class="lang-group-header">
+          <p>${title}</p>
+          <button @click=${() => this.handlePublish(items)}>Publish all</button>
+        </div>
+        <ul class="lang-group-list">${items.map((item) => html`
+          <li class="lang-top-list-item">
+            <p>${item.name}</p>
+            ${item.langs ? this.renderLocaleLangs(item.langs) : html`<div class="lang-button"><button @click=${() => this.handleOpen(item)}>Edit</button></div>`}
+          </li>`)}
+        </ul>
+      </div>
+    `;
+  }
+
+  renderMessage() {
+    return html`<div class="message"><p>${this._message.text}</p></div>`;
+  }
+
+  renderAll() {
+    return html`
+      ${this.renderGroup('Global languages', this._langs)}
+      ${this.renderGroup('Locales', this._locales)}
+      ${this._message && this.renderMessage()}
+    `;
+  }
+
+  render() {
+    return html`${this._langs && this.renderAll()}`;
+  }
+}
+
+customElements.define('nx-locales', NxLocales);
+
+(async function init() {
+  const { context, token, actions } = await DA_SDK;
+  setContext({ ...context, token });
+
+  const nxLocales = document.createElement('nx-locales');
+  nxLocales.org = context.org;
+  nxLocales.site = context.repo;
+  nxLocales.path = context.path;
+  nxLocales.actions = actions;
+
+  document.body.append(nxLocales);
+}());
